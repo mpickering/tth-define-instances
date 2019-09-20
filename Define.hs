14b4d63c2c334bf5f05d73159e453f96a71191c1
Matthew Pickering <matthewtpickering@gmail.com>
2019-09-20 11:37:23 +0100

    problems

---
+++ Define.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+module Define where
+
+import Generics.SOP
+import Generics.SOP.NP
+import qualified GHC.Generics as GHC
+
+import Language.Haskell.TH
+
+type Syntax a = Q (TExp a)
+type SyntaxF = Q :.: TExp
+
+syntactifyList :: [Syntax a] -> Syntax [a]
+syntactifyList [] = [|| [] ||]
+syntactifyList (x : xs) = [|| $$x : $$(syntactifyList xs) ||]
+
+syntactifyNP :: NP (SyntaxF :.: f) xs -> Syntax (NP f xs)
+syntactifyNP Nil = [|| Nil ||]
+syntactifyNP (Comp (Comp x) :* xs) = [|| $$x :* $$(syntactifyNP xs) ||]
+
+class Generic a => GenericSyntax a where
+  sfrom :: Syntax a -> (SOP SyntaxF (Code a) -> Syntax r) -> Syntax r
+  sto   :: SOP SyntaxF (Code a) -> Syntax a
+
+sapply :: Syntax (a -> b) -> Syntax a -> Syntax b
+sapply cf cx = [|| $$cf $$cx ||]
+
+data BinTree a =
+  Tip | Bin (BinTree a) a (BinTree a)
+  deriving (GHC.Generic, Generic, Eq)
+
+instance GenericSyntax (BinTree a) where
+  sfrom treeSyntax k =
+    [|| case $$treeSyntax of
+          Tip       -> $$(k (SOP (Z Nil)))
+          Bin l x r -> $$(k (SOP (S (Z (Comp [|| l ||] :* Comp [|| x ||] :* Comp [|| r ||] :* Nil)))))
+    ||]
+
+  sto (SOP (Z Nil)) = [|| Tip ||]
+  sto (SOP (S (Z (Comp l :* Comp x :* Comp r :* Nil)))) = [|| Bin $$l $$x $$r ||]
+
+
+gcompare :: (GenericSyntax a, All (All Ord) (Code a)) => Syntax (a -> a -> Ordering)
+gcompare =
+  [|| \ x y -> $$(sfrom [|| x ||] (\ x' -> sfrom [|| y ||] (\ y' -> go (unSOP x') (unSOP y')))) ||]
+  where
+    go :: forall xss . All (All Ord) xss => NS (NP SyntaxF) xss -> NS (NP SyntaxF) xss -> Syntax Ordering
+    go (Z x) (Z y) =
+      sapply [|| foldr (<>) EQ ||]
+             (syntactifyList
+             (collapse_NP
+              (czipWith_NP (Proxy @Ord) (\ (Comp a) (Comp b) -> K [|| compare $$a $$b ||])
+              x
+              y)))
+    go (Z _) (S _) = [|| LT ||]
+    go (S _) (Z _) = [|| GT ||]
+    go (S x) (S y) = go x y
---
+++ Use.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Use where
+
+import Define
+
+instance Ord (BinTree Int) where
+  compare = $$(gcompare)
+
+--compareBinTree :: (Ord a, Ord (BinTree a)) => BinTree a -> BinTree a -> Ordering
+--compareBinTree = $$(gcompare)
+
